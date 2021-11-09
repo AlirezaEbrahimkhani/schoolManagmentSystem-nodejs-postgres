6ba98c75efee232662501904219a03ec51b0c958
AlirezaEbrahimkhani <alirezaebrahimkhani1973@gmail.com>
2021-11-09 23:46:37 +0330

    [Feat] student x score average for course y filter

--- controller/student-filter-controller.js
+++ controller/student-filter-controller.js
@@ -3,7 +3,7 @@ const client = require("../config/db");
 const convertResponse = require("../utilities/convert-reponse");
 
 // @desc        class scores average higher than x
-// @route       GET /api/student/filter
+// @route       GET /api/student/filter/average-higher
 // @access      Public
 exports.getClassScoresAverageHigherThanX = asyncHandler(
   async (req, res, next) => {
@@ -11,6 +11,23 @@ exports.getClassScoresAverageHigherThanX = asyncHandler(
     await client.query(
       "select to_json(public.class_scores_average_higher_than_x($1))",
       [average],
+      (err, result) => {
+        const { rows } = result;
+        if (!err) res.status(200).json(convertResponse(rows, "to_json"));
+      }
+    );
+  }
+);
+
+// @desc        student x score average for course y
+// @route       GET /api/student/filter/average-course
+// @access      Public
+exports.getStudentsXScoreAverageForCourseY = asyncHandler(
+  async (req, res, next) => {
+    const { courseName, firstName, lastName } = req.body;
+    await client.query(
+      "select to_json(public.student_x_score_average_for_course_y($1, $2, $3))",
+      [courseName, firstName, lastName],
       (err, result) => {
         const { rows } = result;
         if (!err) res.status(200).json(convertResponse(rows, "to_json"));
--- routes/student-filter-route.js
+++ routes/student-filter-route.js
@@ -2,8 +2,11 @@ const express = require("express");
 const router = express.Router();
 const {
   getClassScoresAverageHigherThanX,
+  getStudentsXScoreAverageForCourseY,
 } = require("../controller/student-filter-controller");
 
-router.get("/", getClassScoresAverageHigherThanX);
+router
+  .get("/average-higher", getClassScoresAverageHigherThanX)
+  .get("/average-course", getStudentsXScoreAverageForCourseY);
 
 module.exports = router;
